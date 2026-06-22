b1d24121c34488dc55383b92533dba84837798cb
Harshit Awasthi <harshitawasthistmarys@gmail.com>
2026-06-22 17:25:43 +0530

    Add vercel.json to fix 404 routing on refresh

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,8 @@ import User from "../models/user.model.js";
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    proxy : true
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
